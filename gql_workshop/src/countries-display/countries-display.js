a54cd133c0db91b3a2a2f877b2b52f769cd84525
David Espinoza <despinoza1027@gmail.com>
2019-08-12 19:47:11 -0500

    update CountriesDisplay for ZeroState

--- gql_workshop/src/countries-display/countries-display.js
+++ gql_workshop/src/countries-display/countries-display.js
@@ -1,32 +1,45 @@
 import React from 'react';
+import feedMeImg from '../assets/feedMe.jpg';
 import {
   currencyIterator,
   languagesIterator,
 } from '../utils/utils';
 
-const CountriesDisplay = ({ countriesData }) => (
-  <React.Fragment>
-    <h1>COUNTRIES</h1>
-    {countriesData.countries.map((country) => (
-      <div
-        className="country-wrapper"
-        key={country.code}
-      >
-        <div>
-          {country.name} {country.emoji}
-        </div>
-        <div style={{ marginTop: '6px' }}>
-          Language(s): {languagesIterator(country.languages)}
-        </div>
-        <div style={{
-          marginTop: '8px',
-          marginBottom: '4px'
-        }}>
-          Currency: {currencyIterator(country.currency)}
-        </div>
+const CountriesDisplay = ({ countriesData }) => {
+  if (countriesData.length === 0) {
+    return (
+      <div className='error-and-loading-wrapper'>
+        <img
+          src={feedMeImg}
+          alt='feed me'
+        />
       </div>
-    ))}
-  </React.Fragment>
-);
+    )
+  }
+  return (
+    <React.Fragment>
+      <h1>COUNTRIES</h1>
+      {countriesData.countries.map((country) => (
+        <div
+          className="country-wrapper"
+          key={country.code}
+        >
+          <div>
+            {country.name} {country.emoji}
+          </div>
+          <div style={{ marginTop: '6px' }}>
+            Language(s): {languagesIterator(country.languages)}
+          </div>
+          <div style={{
+            marginTop: '8px',
+            marginBottom: '4px'
+          }}>
+            Currency: {currencyIterator(country.currency)}
+          </div>
+        </div>
+      ))}
+    </React.Fragment>
+  )
+};
 
 export default CountriesDisplay;
