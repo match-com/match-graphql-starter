88ca952d26f8af439154dad83059fee1cc8b6b25
David Espinoza <despinoza1027@gmail.com>
2019-08-12 20:03:11 -0500

    update Countries Display component for when country items aren't queried for

--- gql_workshop/src/countries-display/countries-display.js
+++ gql_workshop/src/countries-display/countries-display.js
@@ -16,28 +16,35 @@ const CountriesDisplay = ({ countriesData }) => {
       </div>
     )
   }
+
   return (
     <React.Fragment>
       <h1>COUNTRIES</h1>
-      {countriesData.countries.map((country) => (
-        <div
-          className="country-wrapper"
-          key={country.code}
-        >
-          <div>
-            {country.name} {country.emoji}
+      {countriesData.countries.map((country, index) => {
+        const keyIndex = country.code ? country.code : index;
+        const countryName = country.name ? country.name : '-country name missing-';
+        const countryEmoji = country.emoji ? country.emoji : '-country flag missing-';
+
+        return (
+          <div
+            className="country-wrapper"
+            key={keyIndex}
+          >
+            <div>
+              {countryName} {countryEmoji}
+            </div>
+            <div style={{ marginTop: '6px' }}>
+              Language(s): {languagesIterator(country.languages)}
+            </div>
+            <div style={{
+              marginTop: '8px',
+              marginBottom: '4px'
+            }}>
+              Currency: {currencyIterator(country.currency)}
+            </div>
           </div>
-          <div style={{ marginTop: '6px' }}>
-            Language(s): {languagesIterator(country.languages)}
-          </div>
-          <div style={{
-            marginTop: '8px',
-            marginBottom: '4px'
-          }}>
-            Currency: {currencyIterator(country.currency)}
-          </div>
-        </div>
-      ))}
+        )        
+      })}
     </React.Fragment>
   )
 };
